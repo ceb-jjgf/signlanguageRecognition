fa05fcffcd88eba7bd0891a9f9a1ca198d328b97
ceb <eb.cai@computer.com>
2023-12-30 16:40:46 +0800

    add UI for display text about result

--- CameraViewController.swift
+++ CameraViewController.swift
@@ -5,14 +5,53 @@ class CameraViewController: UIViewController, AVCaptureFileOutputRecordingDelega
     var captureSession: AVCaptureSession?
     var videoOutput: AVCaptureMovieFileOutput?
     var previewLayer: AVCaptureVideoPreviewLayer?
+    var messageLabel: UILabel?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupCamera()
+        setupMessageLabel()
     }
 
     func setupCamera() {
-        // ... 省略初始化摄像头的代码 ...
+        captureSession = AVCaptureSession()
+        guard let captureSession = captureSession else { return }
+
+        // 设置摄像头输入
+        guard let videoDevice = AVCaptureDevice.default(for: .video),
+              let videoInput = try? AVCaptureDeviceInput(device: videoDevice) else { return }
+        if captureSession.canAddInput(videoInput) {
+            captureSession.addInput(videoInput)
+        }
+
+        // 设置视频输出
+        videoOutput = AVCaptureMovieFileOutput()
+        if let videoOutput = videoOutput, captureSession.canAddOutput(videoOutput) {
+            captureSession.addOutput(videoOutput)
+        }
+
+        // 设置预览层
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer?.frame = view.bounds
+        previewLayer?.videoGravity = .resizeAspectFill
+        if let previewLayer = previewLayer {
+            view.layer.insertSublayer(previewLayer, at: 0)  // 确保预览层在背景
+        }
+
+        captureSession.startRunning()
+    }
+
+    func setupMessageLabel() {
+        // 初始化并配置 UILabel
+        messageLabel = UILabel()
+        if let messageLabel = messageLabel {
+            messageLabel.frame = CGRect(x: 0, y: view.bounds.height - 60, width: view.bounds.width, height: 40)
+            messageLabel.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+            messageLabel.textColor = .white
+            messageLabel.textAlignment = .center
+            messageLabel.text = "这里到时现实从服务器端处理后的字符串"
+            view.addSubview(messageLabel)
+        }
     }
 
     func startRecording() {
@@ -29,6 +68,7 @@ class CameraViewController: UIViewController, AVCaptureFileOutputRecordingDelega
             // 开始录制
             videoOutput.startRecording(to: fileURL, recordingDelegate: self)
         }
+
     }
 
     // AVCaptureFileOutputRecordingDelegate 的委托方法
@@ -45,5 +85,5 @@ class CameraViewController: UIViewController, AVCaptureFileOutputRecordingDelega
         }
     }
 
-    // 其他必要的方法
+
 }
