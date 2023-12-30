ce6bbf4aa15271ed39da67f4710ae33560a9f4c7
ceb-jjgf <82041142+ceb-jjgf@users.noreply.github.com>
2023-12-30 16:22:30 +0800

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showCamera = false
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                Button("Open Camera") {
+                    showCamera = true
+                }
+                .sheet(isPresented: $showCamera) {
+                    CameraView()
+                }
+
+                Button("Upload Video") {
+                    // 这里添加上传视频的逻辑
+                }
+            }
+        }
+    }
+}
